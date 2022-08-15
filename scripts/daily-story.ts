ef0abdc16a74bda826dfe7e7b42b35ac181854cb
Kent C. Dodds <me+github@kentcdodds.com>
2022-08-15 17:35:15 -0600

    some improvements

---
+++ scripts/daily-story.ts
@@ -0,0 +1,42 @@
+// Menu: Daily Story
+// Description: Write a quick story
+// Author: Kent C. Dodds
+// Shortcut: command option control o
+// Twitter: @kentcdodds
+
+const dateFns = await npm('date-fns')
+const filenamify = await npm('filenamify')
+
+const storyDir = await env(
+  'DAILY_STORY_DIRECTORY',
+  `Where do you want daily stories to be saved?`,
+)
+
+const today = dateFns.format(new Date(), 'yyyy-MM-dd')
+const date = await arg({
+  input: today,
+  hint: 'When did this happen?',
+})
+const title = await arg({
+  placeholder: 'What do you want to call this story?',
+  hint: 'Title',
+})
+
+const md = `---
+title: ${title}
+date: ${date}
+written: ${today}
+---
+
+Write your story here
+`
+
+const filename = filenamify(
+  `${date}-${title.toLowerCase().replace(/ /g, '-').replace(/'/g, '')}.md`,
+  {replacement: '-'},
+)
+const filepath = path.join(storyDir, filename)
+await writeFile(filepath, md)
+await edit(filepath, path.dirname(filepath))
+
+export {}
--- scripts/fix-screens.ts
+++ scripts/fix-screens.ts
@@ -4,8 +4,29 @@
 // Shortcut: command option control f
 // Twitter: @kentcdodds
 
-import "@johnlindquist/kit";
+import '@johnlindquist/kit'
 
-await exec(
-  '/opt/homebrew/bin/displayplacer "id:14EB5859-EA7B-4C0A-BE57-3BEFA8CB0C09 res:2560x1440 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0" "id:3867FCD7-633C-4657-81A4-EBF454F8DD84 res:1920x1080 hz:60 color_depth:8 scaling:on origin:(2560,0) degree:0"'
-);
+const bin = '/opt/homebrew/bin/displayplacer'
+const builtInDisplayId = '37D8832A-2D66-02CA-B9F7-8F30A301B230'
+const leftDisplayId = '14EB5859-EA7B-4C0A-BE57-3BEFA8CB0C09'
+const rightDisplayId = '3867FCD7-633C-4657-81A4-EBF454F8DD84'
+
+const listOutput = (await exec(`${bin} list`)).stdout
+const isDesktop = listOutput.includes(leftDisplayId)
+
+if (isDesktop) {
+  await exec(
+    `${bin} "id:${leftDisplayId} res:2560x1440 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0" "id:${rightDisplayId} res:1920x1080 hz:60 color_depth:8 scaling:on origin:(2560,0) degree:0"`,
+  )
+} else {
+  const is1080p = listOutput.includes(`Resolution: 1920x1080`)
+  if (is1080p) {
+    await exec(
+      `${bin} "id:${builtInDisplayId} res:1728x1117 hz:120 color_depth:8 scaling:on origin:(0,0) degree:0"`,
+    )
+  } else {
+    await exec(
+      `${bin} "id:${builtInDisplayId} res:1920x1080 hz:120 color_depth:8 scaling:on origin:(0,0) degree:0"`,
+    )
+  }
+}
