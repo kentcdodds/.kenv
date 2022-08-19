2732dbcba0a5cc6372e4b7bde85959c1c472e835
Kent C. Dodds <me+github@kentcdodds.com>
2022-08-19 16:53:02 -0600

    fix zoom meeting stuff

--- scripts/create-zoom-meeting.js
+++ scripts/create-zoom-meeting.js
@@ -4,7 +4,7 @@
 // Shortcut: command option control z
 // Twitter: @johnlindquist
 
-import "@johnlindquist/kit";
+import '@johnlindquist/kit'
 
 let help = `
 # Create a Server-to-Server OAuth App
@@ -13,54 +13,62 @@ let help = `
 * Fill out the form
 
 > Scopes - Needs "View and manage all user meetings"
-`;
+`
 
-let ACCOUNT_ID = await env("ZOOM_ACCOUNT_ID", async () => {
+let ACCOUNT_ID = await env('ZOOM_ACCOUNT_ID', async () => {
   return await arg(
     {
-      placeholder: "ZOOM_ACCOUNT_ID",
+      placeholder: 'ZOOM_ACCOUNT_ID',
       ignoreBlur: true,
     },
-    () => md(help)
-  );
-});
-let CLIENT_ID = await env("ZOOM_CLIENT_ID");
-let CLIENT_SECRET = await env("ZOOM_CLIENT_SECRET");
+    () => md(help),
+  )
+})
+let CLIENT_ID = await env('ZOOM_CLIENT_ID')
+let CLIENT_SECRET = await env('ZOOM_CLIENT_SECRET')
 
 let Authorization =
-  "Basic " + Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64");
+  'Basic ' + Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')
 
 let data = new URLSearchParams({
-  grant_type: "account_credentials",
+  grant_type: 'account_credentials',
   account_id: ACCOUNT_ID,
-});
+})
 
-let config = { headers: { Authorization } };
+let config = {headers: {Authorization}}
 
-let response = await post(`https://zoom.us/oauth/token`, data, config);
+let response = await post(`https://zoom.us/oauth/token`, data, config)
 
-let { access_token } = response.data;
+let {access_token} = response.data
+
+const date = new Date()
+const year = date.getFullYear()
+const month = (date.getMonth() + 1).toString().padStart(2, '0')
+const day = date.getDate().toString().padStart(2, '0')
 
 // @see https:
 data = {
-  topic: await arg("Enter your zoom meeting name:"),
-};
+  topic: await arg({
+    hint: 'Zoom meeting name',
+    input: `Meeting with Kent (${year}-${month}-${day})`,
+  }),
+}
 config = {
   headers: {
     Authorization: `Bearer ${access_token}`,
   },
-};
-response = await post(`https://api.zoom.us/v2/users/me/meetings`, data, config);
+}
+response = await post(`https://api.zoom.us/v2/users/me/meetings`, data, config)
 
-let { join_url, id } = response.data;
+let {join_url, pmi, encrypted_password} = response.data
 
 // Copy the URL you're going to share to the clipboard
-await copy(join_url);
+await copy(join_url)
 
 let params = new URLSearchParams({
-  action: "join",
-  confno: id,
-});
+  action: 'join',
+  confno: pmi,
+})
 
 // Opens Zoom using their URL protocol
-await exec(`open 'zoommtg://zoom.us/join?${params}'`);
+await exec(`open 'zoommtg://zoom.us/join?${params}'`)
